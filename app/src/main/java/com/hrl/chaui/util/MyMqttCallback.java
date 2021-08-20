a5dec6c721fcb175a7a710eb59efcc4e4cad7f3c
hzh-dxyl <13544968369@163.com>
2021-08-20 23:14:48 +0800

    添加mqtt回调类

---
+++ app/src/main/java/com/hrl/chaui/util/MyMqttCallback.java
@@ -0,0 +1,27 @@
+package com.hrl.chaui.util;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+public class MyMqttCallback implements MqttCallbackExtended {
+    @Override
+    public void connectComplete(boolean reconnect, String serverURI) {
+
+    }
+
+    @Override
+    public void connectionLost(Throwable cause) {
+
+    }
+
+    @Override
+    public void messageArrived(String topic, MqttMessage message) throws Exception {
+
+    }
+
+    @Override
+    public void deliveryComplete(IMqttDeliveryToken token) {
+
+    }
+}
