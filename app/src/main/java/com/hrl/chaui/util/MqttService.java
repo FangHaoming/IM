59805763d58befa6eb3600893266c4748cd309cb
hzh-dxyl <13544968369@163.com>
2021-08-21 21:28:15 +0800

    完成好友申请mqtt部分

--- app/src/main/java/com/hrl/chaui/util/MqttService.java
+++ app/src/main/java/com/hrl/chaui/util/MqttService.java
@@ -21,7 +21,7 @@ import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 public class MqttService extends Service {
-    private final IBinder mBinder=new LocalBinder();
+    private final IBinder mBinder = new LocalBinder();
     private static final String TAG = "mqttService";
     private MqttByAli mqtt;
     private ArrayList<User> friReqMessage;  //好友申请消息队列
@@ -30,26 +30,27 @@ public class MqttService extends Service {
     public MqttService() {
     }
 
-    public class LocalBinder extends Binder{
-        public MqttService getService(){
+    public class LocalBinder extends Binder {
+        public MqttService getService() {
             return MqttService.this;
         }
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        Log.e(TAG,"onBind");
+        Log.e(TAG, "onBind");
         return mBinder;
     }
 
     @Override
     public void onCreate() {
-        Log.e(TAG,"onCreate");
+        Log.e(TAG, "onCreate");
         SharedPreferences sharedPreferences = getApplication().getSharedPreferences("data", MODE_PRIVATE);
-        int user_id= sharedPreferences.getInt("user_id",-1);
-        String clientId="GID_test@@@"+user_id;
+        int user_id = sharedPreferences.getInt("user_id", -1);
+        friReqMessage = (ArrayList<User>) JSONObject.parseArray(sharedPreferences.getString("friReqMessage", ""), User.class);
+        String clientId = "GID_test@@@" + user_id;
         try {
-            mqtt=new MqttByAli(clientId,"testtopic", new MyMqttCallback());
+            mqtt = new MqttByAli(clientId, "testtopic", new MyMqttCallback());
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -57,16 +58,16 @@ public class MqttService extends Service {
 
     @Override
     public void onDestroy() {
-        SharedPreferences preferences=getApplication().getSharedPreferences("data", MODE_PRIVATE);
-        SharedPreferences.Editor editor= preferences.edit();
-        editor.putString("friReqMessage",JSONObject.toJSONString(friReqMessage));
-        editor.putString("chatMessage",JSONObject.toJSONString(chatMessage));
+        SharedPreferences preferences = getApplication().getSharedPreferences("data", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("friReqMessage", JSONObject.toJSONString(friReqMessage));
+        //editor.putString("chatMessage",JSONObject.toJSONString(chatMessage));
         editor.apply();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.e(TAG,"onStartCommand");
+        Log.e(TAG, "onStartCommand");
         return START_STICKY;
     }
 
@@ -97,25 +98,31 @@ public class MqttService extends Service {
     public class MyMqttCallback implements MqttCallbackExtended {
         @Override
         public void connectComplete(boolean reconnect, String serverURI) {
-            Log.e(TAG,"connectComplete");
+            Log.e(TAG, "connectComplete");
         }
 
         @Override
         public void connectionLost(Throwable cause) {
-            Log.e(TAG,"connectionLost");
+            Log.e(TAG, "connectionLost");
         }
 
         @Override
         public void messageArrived(String topic, MqttMessage message) throws Exception {
-            String payloadString=new String(message.getPayload());
-            JSONObject object= JSONObject.parseObject(payloadString);
-            String msg =object.getString("msg");
-            Log.e(TAG,"messageArrived:"+msg);
-            switch (msg){
-                case "friendRequest":
+            String payloadString = new String(message.getPayload());
+            JSONObject object = JSONObject.parseObject(payloadString);
+            String msg = object.getString("msg");
+            Log.e(TAG, "messageArrived:" + msg);
+            switch (msg) {
+                case "friendRequest":  //好友申请消息
                     //保存信息到文件中
-                    User user=JSONObject.parseObject(payloadString,User.class);
+                    User user = JSONObject.parseObject(payloadString, User.class);
                     friReqMessage.add(user);
+                    break;
+                case "p2pText":  //私聊文本消息
+                    break;
+                case "p2pImg":   //私聊图片消息
+                    break;
+                case "p2pFile":  //私聊文件消息
                     break;
             }
 
@@ -123,7 +130,7 @@ public class MqttService extends Service {
 
         @Override
         public void deliveryComplete(IMqttDeliveryToken token) {
-            Log.e(TAG,"deliveryComplete");
+            Log.e(TAG, "deliveryComplete");
         }
     }
 
